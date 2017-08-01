4b409055c1eeb2db4f8b32483277d4f8f47dc550
just success <956341817@qq.com>
2017-08-01 23:58:20 +0800

    重新生成 dist文件
    重新生成 dist文件

--- dist/static/js/vue.aad5552e93ee4f788736.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([8],{104:function(n,o,p){n.exports=p(2)}},[104]);
-//# sourceMappingURL=vue.aad5552e93ee4f788736.js.map
---
+++ dist/static/js/vue.f225a2f0926557694226.js
@@ -0,0 +1,2 @@
+webpackJsonp([9],{104:function(n,o,p){n.exports=p(2)}},[104]);
+//# sourceMappingURL=vue.f225a2f0926557694226.js.map
