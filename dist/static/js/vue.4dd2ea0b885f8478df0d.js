e9af20cb101c0a3214f972b2af2df51f0222348e
just success <956341817@qq.com>
2017-08-05 17:11:42 +0800

    美化页面 dist

--- dist/static/js/vue.4dd2ea0b885f8478df0d.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([14],{109:function(n,o,p){n.exports=p(2)}},[109]);
-//# sourceMappingURL=vue.4dd2ea0b885f8478df0d.js.map
---
+++ dist/static/js/vue.5648309a892583847938.js
@@ -0,0 +1,2 @@
+webpackJsonp([17],{110:function(n,o,p){n.exports=p(2)}},[110]);
+//# sourceMappingURL=vue.5648309a892583847938.js.map
